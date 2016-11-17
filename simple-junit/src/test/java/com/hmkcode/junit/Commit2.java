7d69015eba69afc2b5ade12b7452044353825cef
Vicky <vicky.thakor@openxcelltechcenter.com>
2016-11-17 06:46:33 +0000

    Conflict resolved

--- simple-junit/src/test/java/com/hmkcode/junit/Commit2.java
+++ simple-junit/src/test/java/com/hmkcode/junit/Commit2.java
@@ -4,6 +4,7 @@ import java.util.function.Consumer;
 
 public class Commit2 {
 	public static void main(String[] args) {
+		System.out.println("Conflict check 2");
 		asdasdaslkdjal skdjalskdj asldjasdas
 	}
 }
