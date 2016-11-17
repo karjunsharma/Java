e6bc2364b9bc84c1700305ba770622d95942613d
Vicky <vicky.thakor@openxcelltechcenter.com>
2016-11-17 06:55:44 +0000

    Conflict
    @karjunsharma @SantoshOpnexcell arjun's conflict resolved 
    "asdasdaslkdjal skdjalskdj asldjasdas"

--- simple-junit/src/test/java/com/hmkcode/junit/Commit2.java
+++ simple-junit/src/test/java/com/hmkcode/junit/Commit2.java
@@ -5,5 +5,6 @@ import java.util.function.Consumer;
 public class Commit2 {
 	public static void main(String[] args) {
 		System.out.println("Conflict check 2");
+		System.out.println("Arjun changes: asdasdaslkdjal skdjalskdj asldjasdas");
 	}
 }
